07cbed63e0c018e095b76c8065918ca29ade5f9c
jesusrdev <102635058+jesusrdev@users.noreply.github.com>
2025-08-28 17:25:02 -0500

    Initial Commit

---
+++ CursoiOS/CursoiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CursoiOSApp.swift
+//  CursoiOS
+//
+//  Created by Jesus Rojas on 28/08/25.
+//
+
+import SwiftUI
+
+@main
+struct CursoiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
