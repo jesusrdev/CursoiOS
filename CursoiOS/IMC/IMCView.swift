04bf47e476d28bc6838482f7c2c9cf6c2bbb7865
jesusrdev <102635058+jesusrdev@users.noreply.github.com>
2025-11-07 23:31:27 -0500

    adding slider component

--- CursoiOS/IMC/IMCView.swift
+++ CursoiOS/IMC/IMCView.swift
@@ -15,6 +15,7 @@ struct IMCView: View {
 //    }
     
     @State var gender: Int = 0
+    @State var height: Double = 150
     
     var body: some View {
         VStack {
@@ -22,6 +23,7 @@ struct IMCView: View {
                 ToggleButton(text: "Hombre", imageName: "heart.fill", gender: 0, selectedGender: $gender)
                 ToggleButton(text: "Hombre", imageName: "star.fill", gender: 1, selectedGender: $gender)
             }
+            HeightCalculator(selectedHeight: $height)
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
             .background(Color("BackgroundApp"))
@@ -41,7 +43,6 @@ struct ToggleButton: View {
     let gender: Int
     
     @Binding var selectedGender: Int
-    
     
     var body: some View {
         let color = gender == selectedGender ? Color("BackgroundComponentSelected") : Color("BackgroundComponent")
@@ -68,6 +69,31 @@ struct InformationText: View {
     
     var body: some View {
         Text(text).font(.largeTitle).bold().foregroundColor(.white)
+    }
+}
+
+struct TitleText: View {
+    let text: String
+    
+    var body: some View {
+        Text(text).font(.title2).foregroundColor(.gray)
+    }
+}
+
+struct HeightCalculator: View {
+    
+    @Binding var selectedHeight: Double
+    
+    var body: some View {
+        VStack {
+            TitleText(text: "Altura")
+            InformationText(text: "\(Int(selectedHeight)) cm")
+            Slider(value: $selectedHeight, in: 100...220, step: 1)
+                .accentColor(.purple)
+                .padding(.horizontal, 16)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color("BackgroundComponent"))
     }
 }
 
